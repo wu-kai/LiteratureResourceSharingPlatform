92d872aa5ea1fe470ec3cc187a879b2fdef7748a
吴凯 <649308287@qq.com>
2018-09-22 16:58:42 +0800

    注册页

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   let $input_box = $('.input_box');
   let $input = $('.input_box input');
+  let $get_code = $('.get_code');
 
   $input.on('focus',function(){
     $(this).parent().addClass('active');
@@ -8,5 +9,30 @@ $(document).ready(function(){
   });
   $input.on('blur',function(){
     $(this).parent().removeClass('active');
+  });
+
+  let isCountDown = false;
+  let timer = null;
+
+  $get_code.on('click',function(){
+    if(isCountDown){
+      return;
+    }else{
+      let count = 10;
+      let oldVal = $get_code.text();
+      $get_code.text(count);
+      isCountDown = true;
+      timer = setInterval(function(){
+        count--;
+        $get_code.text(count);
+        if(count === 0){
+          clearInterval(timer);
+          $get_code.text(oldVal);
+          timer = null;
+          isCountDown = false;
+        }
+      },1000)
+    }
   })
+
 });
