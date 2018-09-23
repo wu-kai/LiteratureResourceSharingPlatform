f56702f3ad0c6b7fea00313de352d8517d32f761
吴凯 <649308287@qq.com>
2018-09-23 17:31:08 +0800

    高级搜索

---
+++ js/advanced_search.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  let $lis = $('.advanced_search_box ul li');
+  let $contents = $('.advanced_search_box .advanced_search_content>div');
+
+  $lis.on('click', function () {
+    let id = $(this).data('id');
+    $(this).addClass('active').siblings().removeClass('active');
+    $contents.eq(id).addClass('active').siblings().removeClass('active')
+  });
+
+
+  let laydate = layui.laydate;
+  let laypage = layui.laypage;
+  let layer = layui.layer;
+
+  let timeArray = [
+    'time01',
+    'time02',
+    'time21',
+    'time22',
+    'time11',
+    'time12',
+    'time31',
+    'time32',
+  ];
+  _.each(timeArray, function (d) {
+    laydate.render({
+      elem: '#' + d //指定元素
+    });
+  });
+
+
+  laypage.render({
+    elem: 'paging',
+    count: 100,
+    groups: 10,
+    layout: ['prev', 'page', 'next', 'count', 'skip'],
+    theme: 'common_paging'
+  });
+
+
+  $('.choose').on('click',function(){
+    layer.open({
+      type:1,
+      title: '国际标准分类',
+      content: $('#modal1'),
+      btn:['确认','取消']
+    });
+
+  })
+});
